30796aaa9adc18fd054803b4bd08d9c155a8ec20
veerleprins <veerleprins@hotmail.com>
2021-04-19 16:14:48 +0200

    Added room & enterGameRoom routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,14 +1,21 @@
 // Externals
 const express = require("express");
 const router = express.Router();
-// const bodyParser = require("body-parser");
-// const urlencodedParser = bodyParser.urlencoded({ extended: true });
+const bodyParser = require("body-parser");
+const urlencodedParser = bodyParser.urlencoded({ extended: true });
 
 // Renders
 const home = require("./renders/home");
+const room = require("./renders/room");
+const enterGameRoom = require("./renders/enterGameRoom");
 const error = require("./renders/error");
 
 // Routes
-router.get("/", home).get("/*", error);
+router
+  .get("/", home)
+  .get("/room/:id", room)
+  .get("/*", error)
+  .post("/room/:id", urlencodedParser, room)
+  .post("/enterRoom", urlencodedParser, enterGameRoom);
 
 module.exports = router;
