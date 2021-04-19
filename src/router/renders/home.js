374a1a6b20b5b6be1dfda6603c8733c6806995c2
veerleprins <veerleprins@hotmail.com>
2021-04-19 16:17:44 +0200

    Added uniqid key

--- src/router/renders/home.js
+++ src/router/renders/home.js
@@ -1,8 +1,12 @@
+// Externals
+const key = require("uniqid");
+
 const home = (req, res) => {
   try {
     // Render the page:
     res.render("index.ejs", {
       pageTitle: "Home",
+      newKey: key(),
     });
   } catch (err) {
     console.error(err);
